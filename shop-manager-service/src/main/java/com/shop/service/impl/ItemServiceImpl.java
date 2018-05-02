a83a03427ec47ebe7ca33987f414a8643dece797
LiLingSheng <2142323400@qq.com>
2018-05-02 17:28:00 +0800

    a

---
+++ shop-manager-service/src/main/java/com/shop/service/impl/ItemServiceImpl.java
@@ -0,0 +1,38 @@
+package com.shop.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.shop.mapper.TbItemMapper;
+import com.shop.pojo.TbItem;
+import com.shop.pojo.TbItemExample;
+import com.shop.pojo.TbItemExample.Criteria;
+import com.shop.service.ItemService;
+
+/**
+ * 商品管理Service
+ */
+@Service
+public class ItemServiceImpl implements ItemService {
+
+	@Autowired
+	private TbItemMapper itemMapper;
+	
+	@Override
+	public TbItem getItemById(long itemId) {
+		
+		//TbItem item = itemMapper.selectByPrimaryKey(itemId);
+		//添加查询条件
+		TbItemExample example = new TbItemExample();
+		Criteria criteria = example.createCriteria();
+		criteria.andIdEqualTo(itemId);
+		List<TbItem> list = itemMapper.selectByExample(example);
+		if (list != null && list.size() > 0) {
+			TbItem item = list.get(0);
+			return item;
+		}
+		return null;
+	}
+}
